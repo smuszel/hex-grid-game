1ae3df847d6261f6c428053da63c1168411d2ab7
smuszel <stanislaw.muszel@gmail.com>
2019-06-20 12:04:55 +0200

    cleanup and bump dependencies

--- webpack.config.js
+++ webpack.config.js
@@ -1,49 +1,42 @@
 const path = require('path');
-const webpack = require('webpack');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const WebpackMd5Hash = require('webpack-md5-hash');
-const CleanWebpackPlugin = require('clean-webpack-plugin');
+const CleanWebpackPlugin = require('clean-webpack-plugin').CleanWebpackPlugin;
 
 const pathTo = {
-    dist: path.join(__dirname, 'dist'),
-    app: path.join(__dirname, 'src', 'index.ts')
-}
+    docs: path.join(__dirname, 'docs'),
+    app: path.join(__dirname, 'src', 'index.ts'),
+    favicon: path.join(__dirname, 'src', 'favicon.ico'),
+    template: path.join(__dirname, 'src', 'index.html'),
+};
 
 module.exports = {
     entry: {
-        main: pathTo.app
+        main: pathTo.app,
     },
-
     resolve: {
-        extensions: ['.ts']
+        extensions: ['.ts'],
     },
-
     output: {
-        path: pathTo.dist,
-        filename: '[name].[hash].js'
+        path: pathTo.docs,
+        filename: '[name].[hash].js',
     },
-
     module: {
         rules: [
             {
                 test: /\.ts$/i,
-                loaders: 'ts-loader'
+                loaders: 'ts-loader',
             },
             {
                 test: /\.scss$/,
-                use: [
-                    MiniCssExtractPlugin.loader,
-                    'css-loader',
-                    'sass-loader'
-                ]
-            }
-        ]
+                use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader'],
+            },
+        ],
     },
-
     plugins: [
-        new CleanWebpackPlugin('dist', {
-            exclude: ['favicon.ico']
+        new CleanWebpackPlugin({
+            exclude: ['favicon.ico'],
         }),
         new MiniCssExtractPlugin({
             filename: 'style.[hash].css',
@@ -51,12 +44,13 @@ module.exports = {
         new HtmlWebpackPlugin({
             inject: false,
             hash: true,
-            template: './src/index.html',
-            filename: 'index.html'
+            template: pathTo.template,
+            favicon: pathTo.favicon,
+            filename: 'index.html',
         }),
-        new WebpackMd5Hash()
+        new WebpackMd5Hash(),
     ],
 
     mode: 'development',
-    devtool: 'eval'
+    devtool: 'eval',
 };
